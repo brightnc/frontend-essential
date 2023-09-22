ac4ed41e36d0778d8ba3a5313dfb34492b210786
Siwat Anandechsak <brightdev@Siwats-MacBook-Pro.local>
2023-09-22 17:44:21 +0700

    modify: script.js

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
-const main = () => {
-  const frontendInput = document.getElementById("frontend-input");
-  const frontendButton = document.getElementById("add-frontend");
+const main = async () => {
+  try {
+    const response = await fetch("https://api.minireg.thanayut.in.th/courses");
+    const rawData = await response.json();
 
-  const frontendMilestoneList = document.getElementById("frontend-milestone");
-
-  frontendButton.addEventListener("click", (e) => {
-    e.preventDefault;
-    if (frontendInput.value === "") {
-      alert("input can't be empty");
-      return;
+    if (!response.ok) {
+      throw new Error(`code: ${response.status}, message: ${rawData.message}`);
     }
-    const newList = document.createElement("li");
-    newList.textContent = frontendInput.value;
-    frontendMilestoneList.appendChild(newList);
-    frontendInput.value = "";
-  });
+    console.log(rawData);
+  } catch (err) {
+    console.log(err);
+  }
 
-  const backendInput = document.getElementById("backend-input");
-  const backendButton = document.getElementById("add-backend");
+  // const frontendInput = document.getElementById("frontend-input");
+  // const frontendButton = document.getElementById("add-frontend");
 
-  const backendMilestoneList = document.getElementById("backend-milestone");
+  // const frontendMilestoneList = document.getElementById("frontend-milestone");
 
-  backendButton.addEventListener("click", (e) => {
-    e.preventDefault;
-    if (backendInput.value === "") {
-      alert("input can't be empty");
-      return;
-    }
-    const newList = document.createElement("li");
-    newList.innerHTML = backendInput.value;
-    backendMilestoneList.appendChild(newList);
-    backendInput.value = "";
-  });
+  // frontendButton.addEventListener("click", (e) => {
+  //   e.preventDefault;
+  //   if (frontendInput.value === "") {
+  //     alert("input can't be empty");
+  //     return;
+  //   }
+  //   const newList = document.createElement("li");
+  //   newList.textContent = frontendInput.value;
+  //   frontendMilestoneList.appendChild(newList);
+  //   frontendInput.value = "";
+  // });
+
+  // const backendInput = document.getElementById("backend-input");
+  // const backendButton = document.getElementById("add-backend");
+
+  // const backendMilestoneList = document.getElementById("backend-milestone");
+
+  // backendButton.addEventListener("click", (e) => {
+  //   e.preventDefault;
+  //   if (backendInput.value === "") {
+  //     alert("input can't be empty");
+  //     return;
+  //   }
+  //   const newList = document.createElement("li");
+  //   newList.innerHTML = backendInput.value;
+  //   backendMilestoneList.appendChild(newList);
+  //   backendInput.value = "";
+  // });
 };
 
 document.addEventListener("DOMContentLoaded", () => {
